305ea43507367f081de0756386185d601fe05c88
Vlad Solov'ev <solovei130694@gmail.com>
2022-09-22 02:13:45 +0300

    Anketa

--- src/index.js
+++ src/index.js
@@ -8,3 +8,88 @@ root.insertAdjacentHTML("beforebegin", Button());
 
 const res = add123(222, 3);
 console.log("root");
+
+//Задание №2
+/*let hash = "";
+for (let i = 0; i < 8; i++) {
+  hash = hash + "\n";
+  for (let j = 0; j < 8; j++) {
+    if ((j + i) % 2 == 0) {
+      hash += "#";
+    } else {
+      hash += " ";
+    }
+  }
+}
+alert(hash);
+*/
+//Задание №3
+
+/*let num;
+do {
+  num = prompt("Введите число больше 100?", 0);
+} while (num <= 100 && num);
+*/
+
+//Домашнее задание
+let surname;
+do {
+  surname = prompt("Ваша фамилия?", "");
+} while (surname == "" || surname == undefined);
+
+let name;
+do {
+  name = prompt("Ваше имя", "");
+} while (name == "" || name == undefined);
+
+let middleName;
+do {
+  middleName = prompt("Ваше отчество", "");
+} while (middleName == "" || middleName == undefined);
+
+let age;
+do {
+  age = prompt("Ваш возраст в годах", "");
+} while (age == "" || age == undefined || age >= 120 || age <= 0);
+
+let ageInDays = 365 * age;
+let ageInFiveYears = 5 + Number(age);
+
+let gender;
+let genderMan = confirm("Ваш пол - мужской?");
+if (genderMan == true) {
+  gender = "мужской";
+} else {
+  gender = "женский";
+}
+
+let youAreRetired = Number(age);
+if (youAreRetired <= 65) {
+  youAreRetired = "нет";
+} else {
+  youAreRetired = "да";
+}
+
+alert(
+  "Ваше ФИО: " +
+    surname +
+    " " +
+    name +
+    " " +
+    middleName +
+    "\n" +
+    "Ваш возраст в годах: " +
+    age +
+    "\n" +
+    "Ваш возраст в днях: " +
+    ageInDays +
+    "\n" +
+    "Через 5 лет вам будет: " +
+    ageInFiveYears +
+    "\n" +
+    "Ваш пол: " +
+    gender +
+    "\n" +
+    "Вы на пенсии: " +
+    youAreRetired
+);
